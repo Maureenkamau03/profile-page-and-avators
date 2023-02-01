1edd5f605fadd6541e8fe6e6c1f915053b64cad3
Maureenkamau03 <mkaymaureen@gmail.com>
2023-02-01 13:22:33 +0300

    added base.html, links and about me page

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,8 @@ from flask import render_template
 def index():
     """Index URL"""
     return render_template('index.html', title='Index Page')
-    
+
+@app.route('/aboutme')  
+def aboutme():
+    'About me URL'
+    return render_template('aboutme.html', title="About me page")  
