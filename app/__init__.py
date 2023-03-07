8ca7aa560503498bd3559ff94d15565eb4549062
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-07 11:47:15 +0300

    added images

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
+
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
 app.config.from_object(Config)
 
-from app import routes 
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 from app.forms import LoginForm
 
+
 @app.route("/")
 @app.route("/index")
 def index():
