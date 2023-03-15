46dfc737da0eec3279100a5e2593edf580190526
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-15 14:11:25 +0300

    added flask-sqlalchemy configurations in config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """Define app conigurations"""
     SECRET_KEY = os.environ.get('SECRET_KEY')  or 'hard-to-guess'
+
+    #db configuration
+    SQLALCHEMY_DATABASE_URI = os.environ.get( 'DATABASE_URL)') or \
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
