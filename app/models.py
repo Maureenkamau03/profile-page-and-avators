b6645627e4c7ec3e04cdf6b46a0c143eae8b9066
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-16 13:24:12 +0300

    Loading a user from the db by id

--- app/models.py
+++ app/models.py
@@ -2,6 +2,13 @@ from datetime import datetime
 from app import db 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from app import db, login
+
+#loading a user from the db using their id
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
