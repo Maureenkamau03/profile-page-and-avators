268f61e14db77851de662c357795833809fe6c44
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-16 14:10:54 +0300

    added register template, added register link to base template and modified login template

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,17 @@ from wtforms.validators import DataRequired, Length
 class LoginForm(FlaskForm):
     '''Login Form'''
     username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
-    language = SelectField('Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')], validators=[DataRequired()])
+   
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    '''Login Form'''
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 120)])
+    
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired()])
+    
+    submit = SubmitField('Register')   
