602fce29d7cebad46893db7d0167fb0e6d076545
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-16 13:17:32 +0300

    added UserMixin class

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 app.config.from_object(Config)
@@ -9,5 +10,6 @@ app.config.from_object(Config)
 #objects
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
