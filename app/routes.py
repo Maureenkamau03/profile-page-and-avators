6f0a972cafb98bfb20f312eb8baf4bcdccc5fb3e
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-16 13:40:35 +0300

    registering a user

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
-from app import app
+from app import app, db
+from app.models import User
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register URL"""
+    form =  RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'User {form.username.data} has been registered successfully.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/")
 @app.route("/home")
