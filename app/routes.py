c25b5416521dadd864aa4308b354fa2abd4800aa
Maureenkamau03 <mkaymaureen@gmail.com>
2023-03-17 12:01:34 +0300

    log user in using login_user function

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
 from app import app, db
 from app.models import User
+from flask_login import login_user
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm, RegisterForm
 
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login URL"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Welcome {form.username.data}')
+        # flash(f'You are requesting to login {form.username.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='login', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -28,13 +45,13 @@ def aboutme():
     'About me URL'
     return render_template('aboutme.html', title="About me page") 
 
-@app.route('/login', methods = ['GET', 'POST'])
-@app.route('/login')
-def login():
-    '''login url'''
-    form = LoginForm()
+# @app.route('/login', methods = ['GET', 'POST'])
+# @app.route('/login')
+# def login():
+#     '''login url'''
+#     form = LoginForm()
 
-    if form.validate_on_submit():
-        flash(f'You are requesting to login {form.username.data}')
-        return redirect(url_for('index'))
-    return render_template('login.html', title='login', form=form)     
+#     if form.validate_on_submit():
+#         flash(f'You are requesting to login {form.username.data}')
+#         return redirect(url_for('index'))
+#     return render_template('login.html', title='login', form=form)     
